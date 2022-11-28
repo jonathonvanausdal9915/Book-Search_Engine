c18ca98d2b15f9b02c603f957fce2734a2081024
Jonathon <>
2022-11-27 21:26:52 -0700

    server for heroku

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ const server = new ApolloServer({
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
